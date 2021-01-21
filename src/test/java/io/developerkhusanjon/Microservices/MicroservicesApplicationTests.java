4703bb3d6b3136c32d4307370124cb68c44d1026
Khusanjon Tuychiboyev <developerkhusanjon@gmail.com>
2021-01-21 20:51:23 +0500

    Zero Stage

---
+++ src/test/java/io/developerkhusanjon/Microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.developerkhusanjon.Microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
